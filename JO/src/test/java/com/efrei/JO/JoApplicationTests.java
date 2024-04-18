e1e29eeb46ac835de0a3cb936a170bea4f28fefb
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2024-04-18 10:00:03 +0200

    init

---
+++ JO/src/test/java/com/efrei/JO/JoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.JO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
