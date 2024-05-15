30b150894cdf500c08753e1090c1d19eb494b431
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2024-05-15 12:12:28 +0200

    add register & login

---
+++ JO/src/main/java/com/efrei/JO/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.efrei.JO.controller;
+
+import com.efrei.JO.dto.LoginDto;
+import com.efrei.JO.dto.LoginResponse;
+import com.efrei.JO.dto.RegisterDto;
+import com.efrei.JO.model.User;
+import com.efrei.JO.service.AuthService;
+import com.efrei.JO.security.JwtService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+	private final JwtService jwtService;
+
+	private final AuthService authenticationService;
+
+	public AuthController(JwtService jwtService, AuthService authenticationService) {
+		this.jwtService = jwtService;
+		this.authenticationService = authenticationService;
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<User> register(@RequestBody RegisterDto registerUserDto) {
+		User registeredUser = authenticationService.signup(registerUserDto);
+		return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponse> authenticate(
+			@RequestBody LoginDto loginUserDto) {
+		User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+		String jwtToken = jwtService.generateToken(authenticatedUser);
+
+		LoginResponse loginResponse = new LoginResponse();
+		loginResponse.setToken(jwtToken);
+		loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+		return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+	}
+}
