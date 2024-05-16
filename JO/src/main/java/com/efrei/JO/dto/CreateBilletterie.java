1a2c11cbc39f2cd4390d5ac7f009efee1b6a6ebd
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2024-05-16 12:03:17 +0200

    Update CreateBilletterie.java

--- JO/src/main/java/com/efrei/JO/dto/CreateBilletterie.java
+++ JO/src/main/java/com/efrei/JO/dto/CreateBilletterie.java
@@ -1,11 +1,5 @@
 package com.efrei.JO.dto;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.efrei.JO.model.Billet;
-
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.*;
 
 public class CreateBilletterie {
@@ -14,16 +8,8 @@ public class CreateBilletterie {
 	@Size(min = 1, max = 40)
 	private String name;
 
-	@Column(nullable = true)
-	private Set<Billet> billets = new HashSet<>();
-
-	public CreateBilletterie(String name, Set<Billet> billets) {
+	public CreateBilletterie(String name) {
 		this.name = name;
-		this.billets = billets;
-	}
-
-	public Set<Billet> getBillets() {
-		return billets;
 	}
 
 	public String getName() {
